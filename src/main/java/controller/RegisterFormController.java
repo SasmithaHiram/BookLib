5b483b26462ea0c83c528ff6c5e778e4ef6c2aa9
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-11 23:34:35 +0530

    first commit

---
+++ src/main/java/controller/RegisterFormController.java
@@ -0,0 +1,27 @@
+package controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class RegisterFormController {
+
+    @FXML
+    private JFXPasswordField txtConfirmPassword;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    void btnRegisterCustomer(ActionEvent event) {
+
+    }
+
+}
