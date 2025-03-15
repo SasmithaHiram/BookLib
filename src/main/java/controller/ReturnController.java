af53f28f7479a3b1c18e67f15506755e1695ccac
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-03-16 01:43:30 +0530

    Return Books

--- src/main/java/controller/ReturnController.java
+++ src/main/java/controller/ReturnController.java
@@ -14,6 +14,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import service.custom.BorrowService;
 import service.custom.ReturnService;
 import service.impl.BookServiceImpl;
@@ -65,10 +66,8 @@ public class ReturnController {
 
     public JFXTextField txtDewDate;
 
-    @FXML
-    void bntAddToListOnAction(ActionEvent event) {
+    ObservableList<Borrow> observableList = FXCollections.observableArrayList();
 
-    }
 
     @FXML
     boolean btnConfirmBorrowingOnAction(ActionEvent event) {
@@ -93,20 +92,23 @@ public class ReturnController {
         Borrow borrow = new Borrow(borrowIdText, txtMemberIdText, txtBorrowDateText, dewDateText, BorrowStatus.RETURNED, borrowDetails);
         borrowService.UpdateBorrowOrder(borrow);
 
+        boolean updateBorrowDetail = new BorrowDetailController().updateBorrowDetail(borrow.getBorrowedBooks());
 
-            boolean updateBorrowDetail = new BorrowDetailController().updateBorrowDetail(borrow.getBorrowedBooks());
+        if (updateBorrowDetail) {
+            new Alert(Alert.AlertType.INFORMATION, "RETURNED SUCCESSFULLY").show();
+            clear();
+        } else {
+            new Alert(Alert.AlertType.ERROR, "THIS BOOK HAS ALREADY BEEN RETURNED").show();
+        }
 
-
-            return updateBorrowDetail;
-
-
+        return updateBorrowDetail;
     }
 
     @FXML
     void btnSearchBorrowIdOnAction(ActionEvent event) {
         Return searchBorrow = returnService.search(txtBorrowId.getText());
 
-        if (searchBorrow!= null) {
+        if (searchBorrow != null) {
             txtMemberId.setText(searchBorrow.getMemberId());
             txtBorrowDate.setText(searchBorrow.getBorrowDate().toString());
             txtDewDate.setText(searchBorrow.getDewDate().toString());
@@ -115,9 +117,18 @@ public class ReturnController {
             observableList.add(searchBorrow.getBookId());
             cmbBooksId.setItems(observableList);
             cmbBooksId.getSelectionModel().selectFirst();
+        } else {
+            new Alert(Alert.AlertType.INFORMATION, "NO BORROW ID FOUND. PLEASE CHECK AND TRY AGAIN").show();
         }
-
     }
 
+    public void clear() {
+        txtBorrowId.clear();
+        txtMemberId.clear();
+        cmbBooksId.setValue(null);
+        txtBorrowDate.clear();
+        txtDewDate.clear();
+        returnDate.setValue(null);
+    }
 }
 
